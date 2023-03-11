236b10ef8e0f712cd8d8dff2c271e4b5cd38cce1
XLBots / <94062864+piroXpower@users.noreply.github.com>
2023-03-11 19:05:20 +0530

    Create alive.py

---
+++ XLBots/plugins/alive/alive.py
@@ -0,0 +1,8 @@
+@User.on_message(command("alive") & filters.me)
+async def alive_(_, message: Message):
+    """ assistant alive"""
+    fuck = await edit_or_reply(message, msg_txt="•••••") 
+    alive_txt = 
+    alive_pic = config.ALIVE_IMG
+    owner_id = config.OWNER_ID
+    
