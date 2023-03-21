813134c58abfd885dcb9c3f51d95838d29329559
TheDeadlyBots / <94062864+piroXpower@users.noreply.github.com>
2023-03-21 08:37:43 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
 API_ID = int(getenv("API_ID")) 
 API_HASH = getenv(API_HASH", "") 
-
+BOT_TOKEN = getenv("BOT_TOKEN", "") 
+STRING_SESSION = getenv("STRING_SESSION", "") 
+ALIVE_PIC = getenv("ALIVE_PIC", "") 
